d5b0a8323a6f9ff54e93df2ffff2dcae0052fc91
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-06-06 17:19:52 -0400

    almost

--- src/App.js
+++ src/App.js
@@ -2,13 +2,67 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
 class App extends Component {
+  state = {
+    pizzas: [],
+    filterType: null,
+    pizzaToEdit: {
+      vegetarian: null,
+    }
+  }
+
+ componentDidMount(){
+   fetch('http://localhost:3000/pizzas')
+   .then(res => res.json())
+   .then(pizzas => {
+     this.setState({
+       pizzas: [...pizzas]
+     })
+   })
+ }  
+
+ editPizza = (event) => {
+   const editPizzaId = event.target.name
+   const index = editPizzaId - 1 
+   let editPizza = this.state.pizzas[index]
+   this.setState({ pizzaToEdit: editPizza})
+ }
+
+
+  change = (event) => {
+    this.setState({ 
+      pizzaToEdit: {...this.state.pizzaToEdit, [event.target.name]: event.target.value}
+     })
+ }
+
+ submitPizza = (event) => {
+   const pizzId = event.target.name
+   fetch(`http://localhost:3000/pizzas/${pizzId}`, {
+     method: 'PATCH',
+     headers: {
+       'content-type': 'application/json',
+       accept: 'application/json'
+     }, body: JSON.stringify(this.state.pizzaToEdit)
+   }).then(res => res.json()).then(pizza => console.log(pizza))
+ }
+
+
+ toggle = (event) => {
+   console.log(event.target.value)
+   this.setState({
+     pizzaToEdit: { ...this.state.pizzaToEdit, vegetarian: event.target.value }
+   })
+ }
+
   render() {
+    let pizzasShown = this.state.pizzas 
+
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizzaToEdit={this.state.pizzaToEdit} change={this.change} submitPizza={this.submitPizza} toggle={this.state.toggle}/>
+        <PizzaList pizzas={pizzasShown} editPizza={this.editPizza}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,13 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input onChange={props.change} name='topping' type="text" className="form-control" placeholder="Pizza Topping" value={props.pizzaToEdit.topping}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select onChange={props.change} name='size' value={props.pizzaToEdit.size} className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,20 +15,20 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input name='vegOrNot' className="form-check-input" type="radio" value='true' checked={props.pizzaToEdit.vegetarian} onChange={props.toggle} />
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input name='vegOrNot' className="form-check-input" type="radio" value="false" checked={!props.pizzaToEdit.vegetarian} onChange={props.toggle}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" name={props.pizzaToEdit.id} className="btn btn-success" onClick={props.submitPizza}>Submit</button>
         </div>
       </div>
 
